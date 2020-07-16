99db4d7ddff5d82d74959f7b84d74e3470e57c9b
Usama Subhani <reachusama@protonmail.com>
2020-07-16 21:07:34 +0500

    Check answers to calculate score

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,26 @@ import React, { useState } from 'react';
 import { fetchQuestions, QuestionState } from './API'
 import Question from './Components/Question'
 
+export type Answer = {
+  correct: boolean;
+  question: string;
+  answer: string;
+  correct_answer: string;
+}
+
 function App() {
 
   const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [no, setNo] = useState(0);
   const [quizInProgress, setProgress] = useState(false);
+  const [userAnswers, setUserAnswers] = useState<Answer[]>([]);
+  const [score, setScore] = useState(0);
   
   const start = async () => { 
     const data = await fetchQuestions();
+    setNo(0);
+    setScore(0);
+    setUserAnswers([]);
     setQuestions(data);
     setProgress(true);
   }
@@ -23,19 +35,45 @@ function App() {
       setProgress(false);
     }
   }
+
+  const checkAnswer = (e: any) => {
+    const answer = e.currentTarget.value;
+    const correct = questions[no].correct_answer == answer;
+    if (correct) {
+      setScore((prev) => prev + 1);
+    }
+    const answerObject = {
+      question: questions[no].question,
+      answer,
+      correct,
+      correct_answer: questions[no].correct_answer,
+    };
+    setUserAnswers((prev) => [...prev, answerObject]);
+    // console.log(userAnswers[no]);
+  }
+
   return (
     <div className="App">
       <h1>QUIZ</h1>
       <button onClick={start}>Start</button>
       {questions.length > 0 && quizInProgress == true ? (
         <div>
+          <p>{ no + 1 }/10</p>
           <Question 
           question={questions[no].question}
           answers={questions[no].answers}
-          questionNo={no+1}/>
+          questionNo={no+1}
+          callBack={checkAnswer}
+          userAnswer={userAnswers ? userAnswers[no] : null}/>
           <button onClick={nextQuestion}>Next</button>
         </div>
           ) : null}
+
+        {quizInProgress == false && userAnswers.length > 0 ? (
+          <div>
+            <h2>Score: { score }</h2>
+          </div>
+        ) : null}
     </div>
   );
 }
--- src/Components/Question.tsx
+++ src/Components/Question.tsx
@@ -1,21 +1,22 @@
 import React from 'react'
+import { Answer } from '../App'
 
 type data = {
-    question: string,
-    answers: string[],
-    questionNo: number,
+    question: string;
+    answers: string[];
+    questionNo: number;
+    callBack: any;
+    userAnswer: Answer | null;
 }
 
-const Question: React.FC<data> = ({ question, answers, questionNo }) => {
+const Question: React.FC<data> = ({ question, answers, questionNo, callBack, userAnswer }) => {
     return (
         <div>
             <p>{ question }</p>
             { answers.map(answer => (
-                <div>
-                    <button>
-                        { answer }
-                    </button>
-                </div>
+                <button key={answer} value={answer} onClick={callBack} disabled={userAnswer ? true:false}>
+                    { answer }
+                </button>
             )) }
         </div>
     )
